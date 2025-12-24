cce496245d0930da4a17becd7b2c854366889332
senkyte <KOH_ZIHAO_ALEXANDER@S2021.SSTS.EDU.SG>
2025-12-24 18:10:16 +0800

    changed a buncha stuff, made EAR more accurate

--- detection/ratios.py
+++ detection/ratios.py
@@ -2,47 +2,46 @@
 # mouth aspect ratio
 
 import math
+import numpy as np
 
 # Indices for landmarks from MediaPipe Face Mesh
 # For eyes we use 6 points each:
 LEFT_EYE_IDX = [33, 160, 158, 133, 153, 144] # clockwise ish ig haha idk
-RIGHT_EYE_IDX = [362, 385, 387, 263, 373, 380]
-# For mouth we use inner upper/lower and corners:
+RIGHT_EYE_IDX = [263, 387, 385, 362, 380, 373]# For mouth we use inner upper/lower and corners:
 MOUTH_TOP = 13     # upper inner lip
 MOUTH_BOTTOM = 14  # lower inner lip
 MOUTH_LEFT = 61    # left corner
 MOUTH_RIGHT = 291  # right corner
 
 def _dist(p1, p2):
-    return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
+    return math.hypot(p1.x - p2.x, p1.y - p2.y)
 
-def eye_aspect_ratio(landmarks, eye_indices):
-    """
-    landmarks: list of (x,y) tuples length 468
-    eye_indices: 6 indices: [p1, p2, p3, p4, p5, p6]
-    returns EAR (float)
-    Formula: (||p2-p6|| + ||p3-p5||) / (2 * ||p1-p4||)
-    """
-    p1 = landmarks[eye_indices[0]]
-    p2 = landmarks[eye_indices[1]]
-    p3 = landmarks[eye_indices[2]]
-    p4 = landmarks[eye_indices[3]]
-    p5 = landmarks[eye_indices[4]]
-    p6 = landmarks[eye_indices[5]]
 
-    vertical1 = _dist(p2, p6)
-    vertical2 = _dist(p3, p5)
-    horizontal = _dist(p1, p4)
-    if horizontal == 0:
+def landmark_to_point(landmark, w, h):
+    return np.array([int(landmark.x * w), int(landmark.y * h)], dtype=np.float32)
+
+def eye_aspect_ratio(landmarks, indices, w, h):
+    """
+    Compute EAR using six landmarks similar to the classic formula:
+    EAR = (||p2 - p6|| + ||p3 - p5||) / (2 * ||p1 - p4||)
+    """
+    pts = [landmark_to_point(landmarks[i], w, h) for i in indices]
+    p1, p2, p3, p4, p5, p6 = pts
+    # vertical distances
+    v1 = np.linalg.norm(p2 - p6)
+    v2 = np.linalg.norm(p3 - p5)
+    # horizontal distance
+    hdist = np.linalg.norm(p1 - p4)
+    if hdist == 0:
         return 0.0
-    ear = (vertical1 + vertical2) / (2.0 * horizontal)
+    ear = (v1 + v2) / (2.0 * hdist)
     return ear
 
-def get_left_ear(landmarks):
-    return eye_aspect_ratio(landmarks, LEFT_EYE_IDX)
+def get_left_ear(landmarks, w, h):
+    return eye_aspect_ratio(landmarks, LEFT_EYE_IDX, w, h)
 
-def get_right_ear(landmarks):
-    return eye_aspect_ratio(landmarks, RIGHT_EYE_IDX)
+def get_right_ear(landmarks, w, h):
+    return eye_aspect_ratio(landmarks, RIGHT_EYE_IDX, w ,h )
 
 def mouth_aspect_ratio(landmarks):
     """
